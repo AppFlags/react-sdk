b5e7cd2d029cc2b9034374f83c8cd4665987b5f6
Justin Capogna <jcapogna@gmail.com>
2023-08-29 16:12:30 +0000

    fix: upgrade appflags to 2.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import AppFlags from "appflags";
 import {useFlag} from "./hooks";
 
+const pkg = require("../package.json");
+AppFlags._setSdk("react", pkg.version);
+
 export default AppFlags;
 export {
     useFlag
